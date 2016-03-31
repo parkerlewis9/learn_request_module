421d27c69b4ae9ce7eaf28a12d70e7e137fa1331
Parker Lewis <parker.lewis14@att.net>
2016-03-31 14:24:20 -0700

    implemented delete one

--- routes/server.js
+++ routes/server.js
@@ -17,26 +17,43 @@ router.route("/beers")
 	.get(function(req, res){
 		request.get(requestUrl + apiKey + "&abv=-10", function(err, response, body){
 			parsedBody = JSON.parse(body);
-			console.log(parsedBody)
 			beerNames = parsedBody.data.map(function(val){
 				return val.name;
 			});
-			res.render("index", {beerNames: beerNames, favorites: []});
+			knex.select("*").from("beers")
+				.then(function(beers){
+					res.render("index", {beerNames: beerNames, favorites: beers});
+					
+				})
+				.catch(function(err){
+					console.log(err);
+				});
 		});
 			// res.render("index", {beerNames: ["Parker's Beer"], favorites: [{name: "Parker", count: 2}]});
 	});
 router.route("/beers/beer")
+// POSSIBLE TODO -  Refactor with GET and req.params
 	.post(function(req, res){
 		request.get(requestUrl + apiKey + '&name=' + encodeURIComponent(req.body.beerSearch), function(err, response, body){
 			parsedBody = JSON.parse(body)
-			// console.log(req.body)
-			// console.log(response)
-			// console.log(parsedBody.data)
 			res.render("show", {info: parsedBody.data[0]})
 			
 		})
 		// res.render("show", {info: {}});
+	})
+	
+
+router.route("/beers/beer/:id")
+	.delete(function(req, res){
+		knex("beers").where("id", parseInt(req.params.id)).del()
+			.then(function(){
+				res.redirect("/beers")
+			})
+			.catch(function(err){
+				console.log(err);
+			});
 	});
+
 router.route("/beers/favorites")
 	// TODO - refactor when I add the fav count feature
 	.post(function(req, res){
